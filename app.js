299c4acfe369c90262c85d64eea21570c7e51eef
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-25 21:46:02 +0100

    Updated app to use new routes.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var todos = require('./routes/todos');
+var todolists = require('./routes/todolists');
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api/users', users);
+app.use('/api/todos', todos);
+app.use('/api/todolists', todolists);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
